7a70a0389f2b51613cb43f6cf88a420b03c5d3e3
Aaron Gertler <aaron.gertler@gmail.com>
2023-11-20 13:00:00 -0600

    chore: first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Logo from './assets/skyview-logo.svg?react'
+
+import './App.css'
+
+function App() {
+  return (
+    <div id='weather-container'>
+      <Logo />
+      <h1>Skyview</h1>
+    </div>
+  )
+}
+
+export default App
