f50e6767606b1d0c18e75dae8578bab4c5ea6700
Aaron Gertler <aaron.gertler@gmail.com>
2023-11-20 17:16:45 -0600

    feat: added weather-info section w/ flexbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,53 @@
 import Logo from './assets/skyview-logo.svg?react'
-
 import './App.css'
 
 function App() {
+  const appVersion = process.env.npm_package_version
+  const currentYear = new Date().getFullYear()
+  const defaultLat = '36.174465'
+  const defaultLon = '-86.767960'
+
   return (
     <div id='weather-container'>
-      <Logo />
-      <h1>Skyview</h1>
+      <header id='weather-header'>
+        <Logo id='main-logo' />
+          <div id='version'>
+            v{appVersion}
+          </div>
+      </header>
+
+      <div id='weather-info'>
+        <div id='current-weather'>
+          <div id='cw-left'>
+            Icon
+          </div>
+          <div id='cw-right'>
+            Info
+          </div>
+        </div>
+
+        <div id='five-day-weather'>
+          <div id='fdw-1'>
+            1
+          </div>
+          <div id='fdw-2'>
+            2
+          </div>
+          <div id='fdw-3'>
+            3
+          </div>
+          <div id='fdw-4'>
+            4
+          </div>
+          <div id='fdw-5'>
+            5
+          </div>
+        </div>
+      </div>
+
+      <footer id='weather-footer'>
+        {`Copyright © ${currentYear} Aaron Gertler. All rights reserved.`}
+      </footer>
     </div>
   )
 }
